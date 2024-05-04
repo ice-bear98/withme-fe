c1bac9cfbaa1f0020b865dc90cb60dee034ed6a8
sueWavy <sueboi0311@gmail.com>
2024-05-04 16:23:44 +0900

    Style : 포스트 페이지 작성

--- src/pages/Event.tsx
+++ src/pages/Event.tsx
@@ -1,3 +1,23 @@
+import PostCard from '../components/PostCard';
+import SearchBar from '../components/SearchBar';
+
 export default function Event() {
-  return <div>Event</div>;
+  return (
+    <div className="mb-10">
+      <SearchBar />
+      <div className="flex justify-center">
+        <div className="mt-7 grid gap-7 grid-cols-2 md:grid-cols-1">
+          <PostCard />
+          <PostCard />
+          <PostCard />
+          <PostCard />
+          <PostCard />
+          <PostCard />
+          <PostCard />
+          <PostCard />
+          <PostCard />
+        </div>
+      </div>
+    </div>
+  );
 }
