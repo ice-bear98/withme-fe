8a79e8a96b1e64c7390b6c8506a43e2a50ca0d9b
sueWavy <sueboi0311@gmail.com>
2024-05-08 22:44:57 +0900

    Fix : branch 이동

--- src/pages/Event.tsx
+++ src/pages/Event.tsx
@@ -1,11 +1,6 @@
 import PostCard from '../components/post/PostCard';
 import SearchBar from '../components/post/SearchBar';
 
-// cherry-pick 해결용 주석
-
-import PostCard from '../components/post/PostCard';
-import SearchBar from '../components/post/SearchBar';
-
 export default function Event() {
   return (
     <div className="mb-10">
