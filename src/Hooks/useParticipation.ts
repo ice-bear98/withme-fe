41a15dc15efb98c4d28675575a84f695c917124a
sueWavy <sueboi0311@gmail.com>
2024-05-15 00:35:07 +0900

    Fix : 검색바 컴포넌트 백엔드 형식 맞게 설정세팅

--- src/Hooks/useParticipation.ts
+++ src/Hooks/useParticipation.ts
@@ -5,13 +5,15 @@ const useParticipation = () => {
   const URL = import.meta.env.VITE_SERVER_URL;
 
   const addParticipation = async (id: any) => {
+    console.log(id);
+
     await axios
       .post(`${URL}/api/participation?gatheringid=${id}`, {
         headers: {
           Authorization: token,
         },
       })
-      .then((res) => console.log(res));
+      .then((res) => console.log('모임 참여 통신 결과 : ', res));
   };
 
   return { addParticipation };
