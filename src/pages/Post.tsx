441f4c717c34794a87ad3ff44c3f6d197268362f
sueWavy <sueboi0311@gmail.com>
2024-05-27 14:22:58 +0900

    Fix : back-up

--- src/pages/Post.tsx
+++ src/pages/Post.tsx
@@ -11,61 +11,35 @@ export default function Post() {
   const location = useLocation();
   const searchParams = new URLSearchParams(location.search);
   const postType = searchParams.get('range');
-  const [currentPage, setCurrentPage] = useState<number>(0);
-  const [pageRange, setPageRange] = useState<number[]>([0, 1, 2, 3, 4]);
 
-  const { data: allData, error: allError, isLoading: isAllLoading } = useGetPost('all', currentPage);
-  const { data: eventData, error: eventError, isLoading: isEventLoading } = useGetPost('event', currentPage);
-  const { data: meetingData, error: meetingError, isLoading: isMeetingLoading } = useGetPost('meeting', currentPage);
+  const { data: allData, error: allError, isLoading: isAllLoading } = useGetPost('all');
+  const { data: eventData, error: eventError, isLoading: isEventLoading } = useGetPost('event');
+  const { data: meetingData, error: meetingError, isLoading: isMeetingLoading } = useGetPost('meeting');
   const { data: searchData, error: searchError, isLoading: isSearchLoading } = useSearchPost(searchQuery);
 
   const [posts, setPosts] = useState<any[]>([]);
-  const [totalPages, setTotalPages] = useState<number>(0);
 
   useEffect(() => {
     if (searchQuery) {
-      setPosts(searchData?.content || []);
-      setTotalPages(searchData?.totalPages || 0);
+      setPosts(searchData || []);
     } else {
-      let data;
       switch (postType) {
         case 'event':
-          data = eventData;
+          setPosts(eventData || []);
           break;
         case 'meeting':
-          data = meetingData;
+          setPosts(meetingData || []);
           break;
         case 'all':
         default:
-          data = allData;
+          setPosts(allData || []);
       }
-      setPosts(data?.content || []);
-      setTotalPages(data?.totalPages || 0);
     }
-    console.log('데이터 다시 불러옴');
   }, [allData, eventData, meetingData, searchData, searchQuery, postType]);
 
   const handleSearch = (query: string) => {
     setSearchQuery(query);
-    setCurrentPage(0);
-    console.log('검색완료');
   };
-
-  useEffect(() => {
-    setCurrentPage(0); // Reset to first page when postType changes
-  }, [postType]);
-
-  const handlePageChange = (page: number) => {
-    setCurrentPage(page);
-  };
-
-  useEffect(() => {
-    const middle = Math.floor(pageRange.length / 2);
-    const newRange = Array.from({ length: 5 }, (_, i) => currentPage - middle + i).filter(
-      (page) => page >= 0 && page < totalPages,
-    );
-    setPageRange(newRange);
-  }, [currentPage, totalPages]);
 
   if (isAllLoading || isEventLoading || isMeetingLoading || isSearchLoading) return <Loader />;
   if (allError || eventError || meetingError || searchError)
@@ -86,31 +60,6 @@ export default function Post() {
             posts.map((it: any) => <PostCard key={it.gatheringId} data={it} />)
           )}
         </div>
-      </div>
-      <div className="flex justify-center mt-10">
-        <button
-          onClick={() => handlePageChange(0)}
-          disabled={currentPage === 0}
-          className="px-3 py-1 mx-1 bg-brand_1 text-white"
-        >
-          처음
-        </button>
-        {pageRange.map((page) => (
-          <button
-            key={page}
-            onClick={() => handlePageChange(page)}
-            className={`px-3 py-1 mx-1 ${page === currentPage ? 'bg-brand_2' : 'bg-brand_3'}`}
-          >
-            {page + 1}
-          </button>
-        ))}
-        <button
-          onClick={() => handlePageChange(totalPages - 1)}
-          disabled={currentPage === totalPages - 1}
-          className="px-3 py-1 mx-1  bg-brand_1 text-white"
-        >
-          마지막
-        </button>
       </div>
     </div>
   );
